9b2a3194bebbd86daee010397ea74cfcc2ac1713
Solomakha-a <135162324+Solomakha-a@users.noreply.github.com>
2023-08-18 21:42:06 +0100

    Added local time option

--- src/index.js
+++ src/index.js
@@ -39,7 +39,11 @@ beijingTimeElement.innerHTML = beijingTimeZoneElement.format("HH:mm:ss [<small>]
 }
 
 function updateCity(event) {
+  
 let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+        cityTimeZone = moment.tz.guess();
+    }
 let cityName = cityTimeZone.replace("_", " ").split("/")[1];
 let cityTime = moment().tz(cityTimeZone);
 let citiesElement = document.querySelector("#cities");
@@ -53,24 +57,6 @@ citiesElement.innerHTML = `
         </div>`;
 
 }
-
-/*function updateCity(event) {
-  let cityTimeZone = event.target.value;
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
-  <div class="city">
-    <div>
-      <h2>${cityName}</h2>
-      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
-    </div>
-    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-    "A"
-  )}</small></div>
-  </div>
-  `;
-}*/
 
 updateTime();
 setInterval(updateTime, 1000);
